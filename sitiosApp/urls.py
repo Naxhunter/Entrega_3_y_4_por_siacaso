30f50dbf07a9464d43f7ce3b9ae6de4ed5970265
Naxhunter <jos.contrerasv@duocuc.cl>
2021-06-17 22:53:18 -0400

    nuevos avances

--- sitiosApp/urls.py
+++ sitiosApp/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('',inicio, name='IND'),
-    path('BASE', base, name='BASE')
+    path('BASE', base, name='BASE'),
+    path('SOLAYU', sol_ayu, name='SOLAYU'),
 ]
--- sitiosApp/views.py
+++ sitiosApp/views.py
@@ -4,5 +4,7 @@ def inicio(request):
     return render(request, 'index.html')
 def base(request):
     return render(request, 'base.html')
+def sol_ayu(request):
+    return render(request, 'sol_ayu.html')
 
 # Create your views here.
