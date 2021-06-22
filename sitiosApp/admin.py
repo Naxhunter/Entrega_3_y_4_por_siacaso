202712c3752039b706df7b08473e1e491e67a66b
Naxhunter <jos.contrerasv@duocuc.cl>
2021-06-21 23:56:04 -0400

    cambios importantes

--- sitiosApp/admin.py
+++ sitiosApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CategoriaUnormal,CategoriaUtrabajador
 
+admin.site.register(CategoriaUtrabajador)
+admin.site.register(CategoriaUnormal)
 # Register your models here.
--- sitiosApp/models.py
+++ sitiosApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class CategoriaUnormal(models.Model):
+    email= models.CharField(primary_key=True, max_length=30)
+    nombre= models.CharField(max_length=30)
+    contrasena= models.CharField(max_length=30)
+    def __str__(self):
+        return self.nombre
+class CategoriaUtrabajador(models.Model):
+    rut= models.CharField(primary_key=True,max_length=30)
+    email= models.CharField(max_length=30)
+    nombre= models.CharField(max_length=30)
+    contrasena= models.CharField(max_length=30)
+    especialidad= models.CharField(max_length=30)
+    telefono= models.IntegerField()
+    def __str__(self):
+        return self.nombre
+
+
